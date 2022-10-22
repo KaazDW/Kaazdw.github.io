0c8af799e234c21e0b9ff3b208fa0428366d4c18
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-10-23 00:39:20 +0200

    modifcation-psuh

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
-
-
-
+function shutdown() {
+    window.close();
+}
 ttty.initTerminal({
     host: document.querySelector("#terminal-api-ttty"),
     prompt: "C:\\Users\\kaazdw > ",
-    //   welcomeMessage: "Welcome",
+    welcomeMessage: "Hi, welcome on my hub terminal. Press 'tab' or click for writing. Use 'help' to display all commands you can use here.",
     commands: {
         echo: {
             name: "echo",
@@ -27,13 +26,13 @@ ttty.initTerminal({
         },
         portfolio: {
             name: "portfolio",
-            description: "a way to go on my personnal website",
-            func: ({ print }) => {( window.open("https://google.fr", "_blank") )}
+            description: "!!WIP!! a way to go on my personnal website ",
+            // func: ({ print }) => {( window.open("https://google.fr", "_blank") )}
         },
         dir: {
             name: "dir",
             description: "a command to display development's informations",
-            func: ({ print }) => { print(">> I build this web-terminal with the Mikhail Korolev's API name TTTY, available on his github: https://github.com/mkrl/ttty. For the background i've used a svg background generator.")}
+            func: ({ print }) => { print(">> I build this web-terminal with the Mikhail Korolev's API name TTTY, available on his github: https://github.com/mkrl/ttty. For the background i've basicly used a svg background generator.")}
         },
         // webhookmsg: {
         //     name: "webhookmsg",
@@ -44,7 +43,12 @@ ttty.initTerminal({
         webhook: {
             name: "webhook",
             description: "a way to go on my webhook discord form",
-            func: ({ print }) => { window.open("webhook/index-webhook.html") }
+            func: ({ load }) => { location.href = "webhook/index-webhook.html" }
+        },
+        github: {
+            name: "github",
+            description: "redirect on my github profile",
+            func: ({ load }) => { window.open("https://github.com/KaazDW") }
         },
         
 
@@ -67,6 +71,4 @@ function Connexion() {
   
   }
   
-function shutdown() {
-    window.close();
-}
+
